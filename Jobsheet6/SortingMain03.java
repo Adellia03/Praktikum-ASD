c6eab9fdca89b458bd2522ed511e19e59ece2a4a
Adellia03 <salsadellia736@gmail.com>
2025-04-05 13:37:40 +0700

    Menambahkan Jobsheet 6

--- Jobsheet6/SortingMain03.java
+++ Jobsheet6/SortingMain03.java
@@ -4,9 +4,11 @@ public class SortingMain03{
 
     int a[]= {20, 10, 2, 7, 12};
     int b[]= {30, 20, 2, 8, 14};
+    int c[]= {40, 10, 4, 9, 3};
 
     Sorting03 dataurut1 = new Sorting03(a, a.length);
     Sorting03 dataurut2 = new Sorting03(b, b.length);
+    Sorting03 dataurut3 = new Sorting03(c, c.length);
 
     System.out.println("Data awal 1");
     dataurut1.tampil();
@@ -20,6 +22,13 @@ public class SortingMain03{
     dataurut2.SelectionSort();
     System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
     dataurut2.tampil();
+    System.out.println("________________________________________________");
+
+    System.out.println("Data awal 3");
+    dataurut3.tampil();
+    dataurut3.insertionSort();
+    System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+    dataurut3.tampil();
 
 }
 }
